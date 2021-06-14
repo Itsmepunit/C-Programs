dd1a4ee37c58aabad19b83491d4ea35bba76aeb8
Itsmepunit <85885693+Itsmepunit@users.noreply.github.com>
2021-06-14 19:31:27 +0530

    Add files via upload

---
+++ sum of two numbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int firstnumber;
+	cin>>firstnumber;
+	
+	int secondnumber;
+	cin>>secondnumber;
+	
+	int sum;
+	sum=firstnumber+secondnumber;
+	
+	cout<<sum;
+	return 0;
+}
